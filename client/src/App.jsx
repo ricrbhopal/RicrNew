e5242c037bf6d10e13f4bf9cc6c17941e76bad36
Raj Vardhan <errvmaurya563@gmail.com>
2025-09-24 13:51:01 +0530

    added tailwind

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,18 +1,11 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useState } from "react";
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
     <>
-    <main>
-
-    </main>
+      <div className="text-9xl">this is app</div>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
