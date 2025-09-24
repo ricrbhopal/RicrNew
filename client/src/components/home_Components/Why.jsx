835572d5c6cc0887c8d17668a40106e3a57e886c
Arunrai0752 <realarunrai0752@gmail.com>
2025-09-24 18:19:42 +0530

    Why Section page is pending

---
+++ client/src/components/home_Components/Why.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+
+
+const Why = () => {
+    return (
+        <div>
+
+            <div className='flex w-full  justify-center items-center gap-6 text-2xl font-bold' >
+                <img src="Starr.png" alt="" height={50} width={50} />
+                <h1 className='text-orange-600'>Coding Excellence Awaits.</h1>
+            </div>
+        </div>
+    )
+}
+
+export default Why
--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -1,11 +1,13 @@
 import React from 'react'
 import Hero from '../components/home_Components/Hero'
+import Why from '../components/home_Components/Why'
 
 const Home = () => {
   return (
     <div>
         
         <Hero/>
+        <Why/>
 
 
         <div>
