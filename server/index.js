691bd9fa7200d737a6fba682f9edf8dbf544e907
vp1472745 <vineetpancheshwar1611@gmail.com>
2025-11-03 16:56:49 +0530

    feat: Update API URLs to use HTTPS for development environment

--- server/index.js
+++ server/index.js
@@ -8,7 +8,7 @@ app.use(cors({
   origin:
     `${process.env.NODE_ENV === 'production'
       ? process.env.FRONTEND_URL
-      : 'http://localhost:5173'}`,
+      : 'https://localhost:5173'}`,
   credentials: true
 }));
 
