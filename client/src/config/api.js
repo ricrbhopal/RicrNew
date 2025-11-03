dc32173f88a83d6fc3a69e87d4cf3314693fa3ff
vp1472745 <vineetpancheshwar1611@gmail.com>
2025-11-03 17:17:59 +0530

    fix: Update API URLs to use HTTP for development environment

--- client/src/config/api.js
+++ client/src/config/api.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const BASE_URL = import.meta.env.VITE_API_BASE_URL || 'https://localhost:4500';
+const BASE_URL = import.meta.env.VITE_API_BASE_URL || 'http://localhost:4500';
 
 const api = axios.create({
     baseURL: BASE_URL,
--- server/index.js
+++ server/index.js
@@ -7,8 +7,9 @@ const PORT = process.env.PORT || 5000;
 app.use(cors({
   origin:
     `${process.env.NODE_ENV === 'production'
-      ? process.env.FRONTEND_URL
-      : 'https://localhost:5173'}`,
+      ? process.env.FRONTEND_URL||'https://hackathon.ricr.in'
+      : 'http://localhost:5173'
+    }`,
   credentials: true
 }));
 
