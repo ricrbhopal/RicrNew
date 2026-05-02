975b4de24e4e2a426168ffa3a04e7fa213594ae2
vineet-pancheshwar_rdpl <vineet.pancheshwar@rajdigital.com>
2026-05-02 17:50:43 +0530

    fix: Increase API request timeout to improve handling of long-running requests

--- client/src/config/api.js
+++ client/src/config/api.js
@@ -4,7 +4,7 @@ const BASE_URL = import.meta.env.VITE_API_URL;
 
 const api = axios.create({
   baseURL: BASE_URL,
-  timeout: 80000 ,
+timeout: 180000 ,
   headers: {
     "Content-Type": "application/json",
   },
